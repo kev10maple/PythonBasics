cd8954281dea66d44bded0c0277b2c2ed1eca029
kev10maple <Kevin.Wang@smoothstack.com>
2021-11-17 12:57:25 -0500

    commiting numpy homework

---
+++ NumpyHW.py
@@ -0,0 +1,30 @@
+#Question 1: to import np
+import numpy as np
+
+print("Question 2")
+print(np.zeros(10))
+
+print("\nQuestion 3")
+print(np.ones(10))
+
+print("\nQuestion 4")
+print(np.ones(10) * 5)
+
+print("\nQuestion 5")
+print(np.arange(10, 51))
+
+print("\nQuestion 6")
+print(np.arange(10, 51, 2))
+
+print("\nQuestion 7")
+print(np.arange(9).reshape(3, 3))
+
+print("\nQuestion 8")
+print(np.eye(3))
+
+print("\nQuestion 9")
+print(np.random.rand(1))
+
+
+
+
